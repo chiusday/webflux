372b67b627cab2fd4a83a80f4b412159e89ce724
RicardoVCDev <gowfourth@hotmail.com>
2020-01-18 17:45:53 +0800

    Insert Relationship in DB successfully

---
+++ process-library/src/main/java/jon/webflux/pdl/router/WorkRouterHandler.java
@@ -0,0 +1,27 @@
+package jon.webflux.pdl.router;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import jon.webflux.pdl.model.ProcessRelationship;
+import jon.webflux.pdl.service.WorkService;
+import reactor.core.publisher.Mono;
+
+@Component
+public class WorkRouterHandler {
+	@Autowired
+	private WorkService workService;
+	
+	public Mono<ServerResponse> addRelationship(ServerRequest request) {
+		return request.bodyToMono(ProcessRelationship.class)
+				.flatMap((ProcessRelationship relationship) -> {
+					return ServerResponse.ok()
+							.contentType(MediaType.APPLICATION_JSON)
+							.body(workService.addRelationship(relationship), 
+									ProcessRelationship.class);
+				});
+	}
+}
