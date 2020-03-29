63b29c259ce5dac09dc589458e860a225bf51e72
RicardoVCDev <gowfourth@hotmail.com>
2020-03-29 20:36:49 +0800

    Use flatmap

--- process-library/src/main/java/jon/webflux/pdl/service/WorkService.java
+++ process-library/src/main/java/jon/webflux/pdl/service/WorkService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jon.webflux.pdl.model.Process;
 import jon.webflux.pdl.model.ProcessRelationship;
@@ -26,19 +27,23 @@ public class WorkService {
 		return relationshipRepository.save(relationship);
 	}
 	
+	@Transactional
 	public Mono<Work> addWork(Work work) {
 		return processRepository.save(new Process(work))
-			.flatMap(process -> {
-				return Flux.just(work.getSteps().stream().toArray(Step[]::new))
-				.flatMap(step -> {
-					step.getRelationship().setParentId(process.getId());
-					return addRelationship(step.getRelationship());
-				})
-				.flatMap(r -> processRepository.findById(r.getChildId())
-						.map(p -> new Step(r.getSequence(), p, r)))
-				.collect(Collectors.toSet())
-				.map(steps -> new Work(process, new TreeSet<Step>(steps)));
-			});
+				.flatMap(process -> {
+					return Flux.just(work.getSteps().stream().toArray(Step[]::new))
+							.flatMap(step -> {
+								step.getRelationship().setParentId(process.getId());
+								return relationshipRepository.save(step.getRelationship());
+							})
+							.flatMap((ProcessRelationship r) -> processRepository.findById(r.getChildId())
+									.flatMap((Process p) -> { 
+										return Mono.just(new Step(r.getSequence(), p, r));
+									}))
+							.collect(Collectors.toSet())
+							.flatMap(steps -> {
+								return Mono.just(new Work(process, new TreeSet<Step>(steps)));
+							});
+					});		
 	}
-	
 }
